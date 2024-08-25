cf68a7233e27ecbfbfea0a132c4f6c78cbaa0ddc
LuisSerra2 <luisserra.work@gmail.com>
2024-08-25 22:26:38 +0100

    Boss smash

--- Assets/_Scripts/Enemies/SmashBall.cs
+++ Assets/_Scripts/Enemies/SmashBall.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,16 +10,16 @@ public class SmashBall : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("asdjasidjasoidj");
         if (collision.gameObject.CompareTag("Ground"))
         {
-
-            
             Vector3 initialPosition = collision.transform.position;
+            Color initialColor = collision.transform.GetComponent<MeshRenderer>().material.color;
+
+            collision.transform.GetComponent<MeshRenderer>().material.color = Color.white;
 
             collision.transform.DOMove(new Vector3(collision.transform.position.x, collision.transform.position.y + 2, collision.transform.position.z), .2f).OnComplete(() =>
             {
-                collision.transform.DOMove(initialPosition, 1f);
+                collision.transform.DOMove(initialPosition, .2f).OnComplete(() => collision.transform.GetComponent<MeshRenderer>().material.color = initialColor);
             });
         }
     }
